01753a5f6af8dc9a6bcb08fac7c39a170f399348
Ajaykumar-0303 <106304929+Ajaykumar-0303@users.noreply.github.com>
2022-05-26 13:59:07 +0530

    Update and rename Multiple Ring Buffer to Multiple Ring Buffer.py

--- Multiple Ring Buffer.py
+++ Multiple Ring Buffer.py
@@ -2,3 +2,4 @@ arcpy.env.workspace = "F:\edu\GIS_lab\Punjab\Punjab_dist"
 int = out
 out1 = "Multi_buff_rail.shp"
 arcpy.MultipleRingBuffer_analysis(int, out1, [100, 200, 300], "meters", "", "ALL")
+print arcpy.GetMessages()
