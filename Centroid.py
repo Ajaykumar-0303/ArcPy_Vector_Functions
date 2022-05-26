acfcc4318e55af9ff2cee7d21b3e294def76d5ca
Ajaykumar-0303 <106304929+Ajaykumar-0303@users.noreply.github.com>
2022-05-26 14:14:54 +0530

    Update Centroid.py

--- Centroid.py
+++ Centroid.py
@@ -1,6 +1,6 @@
 import arcpy
 arcpy.env.workspace = "F:\edu\GIS_lab\Punjab\Punjab_dist"
-inp = out
+inp = "Ludhiana_identity.shp"
 out1 = "Ludhiana_cent.shp"
 arcpy.FeatureToPoint_management(inp, out1, "CENTROID")
 print arcpy.GetMessages()
