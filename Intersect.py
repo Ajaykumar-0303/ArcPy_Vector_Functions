ee2bfc630615d8fdcaab3523b702b06350c753b0
Ajaykumar-0303 <106304929+Ajaykumar-0303@users.noreply.github.com>
2022-05-26 13:58:34 +0530

    Update and rename Intersect to Intersect.py

--- Intersect.py
+++ Intersect.py
@@ -3,3 +3,4 @@ arcpy.env.workspace = "F:\edu\GIS_lab\Punjab"
 input = "Rail.shp", "Ludhiana.shp"
 output = "Intersection.shp"
 arcpy.Intersect_analysis(["Rail.shp", "Ludhiana.shp"], "Intersection.shp", "ALL", "", "INPUT")
+print arcpy.GetMessages()
