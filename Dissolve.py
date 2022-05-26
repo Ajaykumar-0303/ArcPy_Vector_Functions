034b131a23d74fd99b2f8acae810fdbe80142abc
Ajaykumar-0303 <106304929+Ajaykumar-0303@users.noreply.github.com>
2022-05-26 13:57:29 +0530

    Update and rename Dissolve to Dissolve.py

--- Dissolve.py
+++ Dissolve.py
@@ -3,3 +3,4 @@ arcpy.env.workspace = "F:\edu\GIS_lab\Punjab"
 input = "Schools.shp"
 output = "Schools1.shp"
 arcpy.Dissolve_management(input, output, "", "", "SINGLE_PART", "")
+print arcpy.GetMessages()
