7efb66c7758f9770c498cd7b19854309e7ef5d34
Ajaykumar-0303 <106304929+Ajaykumar-0303@users.noreply.github.com>
2022-05-26 13:55:37 +0530

    Update and rename Clip to Clip.py

--- Clip.py
+++ Clip.py
@@ -3,3 +3,4 @@ arcpy.env.workspace = "F:\edu\GIS_lab\Punjab"
 inp = r"F:\edu\GIS_lab\Punjab\Punjab_dist.shp"
 clip = r"F:\edu\GIS_lab\Punjab\Ludhiana.shp"
 arcpy.Clip_analysis(inp, clip, "F:\edu\GIS_lab\Punjab\Ludik.shp")
+print arcpy.GetMessages()
